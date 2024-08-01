d9d96dc4a86406ee3ee7a598c461d01150aef5b3
pedro.cavallaro <pedro.cavallaro@devnology.com.br>
2024-08-01 17:36:12 -0300

    feat: init

---
+++ src/services/jwt.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import * as jwt from 'jsonwebtoken';
+
+@Injectable()
+export class JwtService {
+  signToken(id: string, email: string, name: string, imageUrl: string): string {
+    return jwt.sign(
+      {
+        id,
+        email,
+        name,
+        imageUrl
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: 30 * 60 * 60 * 60
+      }
+    );
+  }
+
+  verifyToken(token: string) {
+    return jwt.verify(token, process.env.JWT_SECRET);
+  }
+}
